a6214fa68b9a8874f414ae075e17602796e54f4f
Tom Chamberlin <tomchamberlin@Toms-MacBook-Air.local>
2024-03-14 22:34:54 +0000

    final adjustments

--- tutor/src/Components/Body/Body.js
+++ tutor/src/Components/Body/Body.js
@@ -1,5 +1,8 @@
 import React from "react";
 import tutor from "../../Images/Tutor4.png";
+import thumbsup from "../../Images/thumbsup.png";
+import stars from "../../Images/stars.png";
+
 import "./Body.css";
 import CustomButton from "../Common/CustomButton/CustomButton";
 
@@ -16,6 +19,20 @@ const Body = () => {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
+          <div className="thumbsUpContainer">
+            <img className="thumbsUp" src={thumbsup} />
+            <div className="happyCustomersContainer">
+              <p className="individualTextBody">500+</p>
+              <p className="individualTextBodySmaller">Happy Customers</p>
+            </div>
+            <div className="happyCustomersContainer">
+              <p className="individualTextBody">5/5</p>
+              <div className="starsContainer">
+                <img className="stars" src={stars} />
+                <p className="individualTextBodySmaller">Rating</p>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="containerRightBody">
           <img className="tutorImage" src={tutor} alt="tutor" />
